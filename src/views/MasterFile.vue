c0c59ed1cae7a0f39c6e3ef246a5c6b618e55aff
devsozib <mdsajibhassa01993@gmail.com>
2022-07-05 09:43:49 +0600

    minify all js file and include on one file

--- src/views/MasterFile.vue
+++ src/views/MasterFile.vue
@@ -3,14 +3,7 @@
     <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         Start Preloader
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~--> 
-    <div class="preloader">
-        <div class="preloader-inner">
-            <div class="preloader-icon">
-                <span></span>
-                <span></span>
-            </div><!-- /preloader-icon -->
-        </div><!-- /preloader-inner -->
-    </div><!-- /preloader -->
+  
 
 
     <!-- header-section start -->
